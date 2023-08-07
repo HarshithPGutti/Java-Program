be28b7b92b883b7ead77a94c22be80cdf4fabcc3
HarshithPGutti <harshithpgutti.xworkz@gmail.com>
2023-08-07 22:37:43 +0530

    chaining

---
+++ 04-08-23/src/com/xworkz/app/Ground/Ground.java
@@ -0,0 +1,5 @@
+package com.xworkz.app.Ground;
+
+public class Ground {
+
+}
---
+++ 04-08-23/src/com/xworkz/app/Route/AirtelRouter.java
@@ -0,0 +1,13 @@
+package com.xworkz.app.Route;
+
+public class AirtelRouter {
+	public AirtelRouter() {
+		super();
+		System.out.println("invoking no-arg const in AirtelRouter");
+	}
+
+	public AirtelRouter(String purpose,String name,int speed,double price)
+	{
+		System.out.println("invoking String,String,int,double const in AirtelRouter");
+	}
+}
